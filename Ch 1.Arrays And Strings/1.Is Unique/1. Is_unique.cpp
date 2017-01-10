80e47b064340d828221758400c7b6e9ab87e1b45
Sanjeev S <sanjeevsuresh1995@gmail.com>
2017-01-10 12:25:44 +0530

    Additional implementations to Q1_1

--- Ch 1.Arrays And Strings/1.Is Unique/1. Is_unique.cpp
+++ Ch 1.Arrays And Strings/1.Is Unique/1. Is_unique.cpp
@@ -1,34 +1,64 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <bitset>
 using namespace std;
 
-bool isUniqueChars(const string &str)
-	{
-		if (str.length() > 128)
-		{
+bool isUniqueChars(const string &str){
+		if (str.length() > 128){
 			return false;
 		}
 		vector<bool> char_set(128);
-		for (int i = 0; i < str.length(); i++)
-		{
+		for (int i = 0; i < str.length(); i++){
 			int val = str[i];
-			if (char_set[val])
-			{
+			if (char_set[val]){
 				return false;
 			}
 			char_set[val] = true;
 		}
 		return true;
-	}
+}
 
-int main()
-	{
+bool isUniqueChars_bitvector(const string &str) {
+	//Reduce space usage by a factor of 8 using bitvector. 
+	//Each boolean otherwise occupies a size of 8 bits.
+	bitset<255> bits(0);
+	for(int i = 0; i < str.length(); i++) {
+		int val = str[i];
+		if(bits.test(val) > 0) {
+			return false;
+		}
+		bits.set(val);
+	}
+	return true;
+}
+bool isUniqueChars_noDS(const string &str) {
+	for(int i = 0; i < str.length()-1; i++) {
+		for(int j = i+1; j < str.length(); j++) {
+			if(str[i] == str[j]) {
+				return false;
+			}
+		}
+	}
+	return true;	
+}
+
+int main(){
 		vector<string> words = {"abcde", "hello", "apple", "kite", "padle"};
 		for (auto word : words)
 		{
 			cout << word << string(": ") << boolalpha << isUniqueChars(word) <<endl;
 		}
+		cout <<endl << "Using bit vector" <<endl;
+		for (auto word : words)
+		{
+			cout << word << string(": ") << boolalpha << isUniqueChars_bitvector(word) <<endl;
+		}
+		cout <<endl << "Using no Data Structures" <<endl;
+		for (auto word : words)
+		{
+			cout << word << string(": ") << boolalpha << isUniqueChars_bitvector(word) <<endl;
+		}
 		return 0;
-	}
+}
 
