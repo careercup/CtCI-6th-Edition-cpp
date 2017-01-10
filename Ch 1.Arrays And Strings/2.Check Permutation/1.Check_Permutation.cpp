b7dc650ce8fb24219a8f600605ec6d8fd1f97a9a
Sanjeev S <sanjeevsuresh1995@gmail.com>
2017-01-10 17:15:58 +0530

    Additional implementation for Q1_2

--- Ch 1.Arrays And Strings/2.Check Permutation/1.Check_Permutation.cpp
+++ Ch 1.Arrays And Strings/2.Check Permutation/1.Check_Permutation.cpp
@@ -3,8 +3,7 @@
 #include <algorithm>
 #include <iostream>
 using namespace std;
-
-bool arePermutation(string &str1,string &str2)
+bool arePermutation(string str1,string str2)
 {
     // Get lengths of both strings
     int n1 = str1.length();
@@ -26,16 +25,51 @@ bool arePermutation(string &str1,string &str2)
     return true;
 }
 
-
-int main()
-{
+bool arePermutation_2(const string &str1, const string &str2) {
+  if(str1.length() != str2.length()) 
+    return false;
+  int count[256]={0};
+  for(int i = 0; i < str1.length(); i++) {
+    int val = str1[i];
+    count[val]++;
+  }
+  for(int i = 0; i < str2.length(); i++) {
+    int val = str2[i];
+    count[val]--;
+    if(count[val]<0) 
+      return false;
+  }
+  return true;
+}
+int main() {
+// Test Method 1 - Using sort
+    cout << "Method 1 - Using sort" << endl;
     string str1 = "testest";
     string str2 = "estxest";
-    if (arePermutation(str1, str2))
-      cout << ("The two strings are permutation of each other") << endl;
+    if(arePermutation(str1, str2))
+      cout << str1 <<" and " << str2 << " are permutation of each other" << endl;
     else
-      cout << ("The two strings are not permutation of each other") << endl;
-    cout << "The sorted first string is " << str1 <<endl;
-    cout << "The sorted second string is " << str2 <<endl;
+      cout << str1 <<" and " << str2 << " are not permutation of each other" << endl;
+    str1 = "hello";
+    str2 = "oellh";
+    if(arePermutation(str1, str2))
+      cout << str1 <<" and " << str2 << " are permutation of each other" << endl;
+    else
+      cout << str1 <<" and " << str2 << " are not permutation of each other" << endl;
+
+//Test Method 2 - Using character count
+    cout << "Method 2 - Using character count" << endl;
+    str1 = "testest";
+    str2 = "estxest";
+    if(arePermutation_2(str1, str2))
+      cout << str1 <<" and " << str2 << " are permutation of each other" << endl;
+    else
+      cout << str1 <<" and " << str2 << " are not permutation of each other" << endl;
+    str1 = "hello";
+    str2 = "oellh";
+     if(arePermutation_2(str1, str2))
+      cout << str1 <<" and " << str2 << " are permutation of each other" << endl;
+    else
+      cout << str1 <<" and " << str2 << " are not permutation of each other" << endl;
     return 0;
 }
