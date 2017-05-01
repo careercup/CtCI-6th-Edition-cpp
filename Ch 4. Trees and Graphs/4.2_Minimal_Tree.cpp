ee543a3fec56254828be97046dd0abbfa172042a
Endilie Yacop Sucipto <endiliey@gmail.com>
2017-05-01 19:46:41 +0800

    Update 4.2_Minimal_Tree.cpp
    Remove redundant comment, simplified solution

--- Ch 4. Trees and Graphs/4.2_Minimal_Tree.cpp
+++ Ch 4. Trees and Graphs/4.2_Minimal_Tree.cpp
@@ -1,23 +1,3 @@
-/*Minimal Tree: Given a sorted (increasing order) array with unique integer elements, write an
-algorithm to create a binary search tree with minimal height.
-*/
-
-// Assume our tree is defined like this
-struct TreeNode {
-	TreeNode* left;
-	TreeNode* right;
-	int data;
-	TreeNode(int a) {
-		data = a;
-	}
-};
-
-// Algorithm wise is:
-// 1. Insert into tree middle element of array
-// 2. Insert into left subtree left subarray element
-// 3. Insert into right subtree right subarray elements
-// 4. Recurse
-
 TreeNode* createMinBST(vector<int> arr) {
 	return createMinBST(arr, 0, arr.length() - 1);
 }
@@ -26,7 +6,6 @@ TreeNode* createMinBST(vector<int> arr, int start, int end) {
 	if (start > end) {
 		return null;
 	}
-
 	int mid = start + (end - start) / 2;
 	TreeNode* newNode = new TreeNode(arr[mid]);
 	newNode.left = createMinBST(arr, start, mid - 1);
