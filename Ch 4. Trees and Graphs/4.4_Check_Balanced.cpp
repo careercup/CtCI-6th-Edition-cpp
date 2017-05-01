9f5f8d3ae460cf9e857264cb642ad626c56d9498
Endilie Yacop Sucipto <endiliey@gmail.com>
2017-05-01 19:47:05 +0800

    Update 4.4_Check_Balanced.cpp
    Remove redundant comment, simplified solution

--- Ch 4. Trees and Graphs/4.4_Check_Balanced.cpp
+++ Ch 4. Trees and Graphs/4.4_Check_Balanced.cpp
@@ -1,18 +1,3 @@
-/*
-Check Balanced: Implement a function to check if a binary tree is balanced. For the purposes of
-this question, a balanced tree is defined to be a tree such that the heights of the two subtrees of any
-node never differ by more than one.
-*/
-
-struct TreeNode {
-	TreeNode* left;
-	TreeNode* right;
-	int data;
-	TreeNode(int a) {
-		data = a;
-	}
-};
-
 int checkHeight(TreeNode* root) {
 	if (root == nullptr) {
 		return 0;
