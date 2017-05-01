f0324753a3de79f143ae220f7442383b585a7ca0
Endilie Yacop Sucipto <endiliey@gmail.com>
2017-05-01 19:47:49 +0800

    Update 4.6_Successor.cpp
    Remove redundant comment, simplified solution

--- Ch 4. Trees and Graphs/4.6_Successor.cpp
+++ Ch 4. Trees and Graphs/4.6_Successor.cpp
@@ -1,18 +1,3 @@
-/*
-Successor: Write an algorithm to find the "next" node (i.e., in-order successor) of a given node in a
-binary search tree. You may assume that each node has a link to its parent.
-*/
-
-struct TreeNode {
-	TreeNode* left;
-	TreeNode* right;
-	TreeNode* parent;
-	int data;
-	TreeNode(int a) {
-		data = a;
-	}
-};
-
 TreeNode* mostLeft(TreeNode* root) {
 	while (root->left != nullptr) {
 		root = root->left;
@@ -20,15 +5,10 @@ TreeNode* mostLeft(TreeNode* root) {
 	return root;
 }
 
-
 TreeNode* findSuccessor(TreeNode* root) {
-	
-	// Go to most left of right child
 	if (root->right != nullptr) {
 		return mostLeft(root->right);
 	}
-
-	// Go to parent until you are the left child
 	TreeNode* child = root;
 	TreeNode* ancestor = root->parent;
 	while(ancestor != nullptr && ancestor->right == child) {
