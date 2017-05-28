74e1556cd210daa5ede296ead16d0b88650f0f84
Doug de Jesus <douglasdejesus92@gmail.com>
2017-05-28 14:51:00 -0700

    Added more robust test cases. Added many test cases to increase coverage, including empty strings, whitespace, uppercase letters, numbers, special characters, and characters above 128. Also made some formatting more consistent.

--- Ch 1.Arrays And Strings/1.Is Unique/1. Is_unique.cpp
+++ Ch 1.Arrays And Strings/1.Is Unique/1. Is_unique.cpp
@@ -12,14 +12,15 @@ booleans to track which characters have been used. The time complexity is O(n),
 as the string only needs to be traversed once. It uses O(k) additional space for
 the array, where k is the alphabet size. */
 bool isUniqueChars(const string &str){
-	if (str.length() > ALPHABET_SIZE){
+	if (str.length() > ALPHABET_SIZE) {
 		return false;
 	}
-
+	
 	bool char_set[ALPHABET_SIZE];
-	for (int i = 0; i < str.length(); i++){
+	for (int i = 0; i < str.length(); i++) {
 		int val = str[i];
-		if (char_set[val]){
+
+		if (char_set[val]) {
 			return false;
 		}
 		char_set[val] = true;
@@ -36,9 +37,9 @@ bool isUniqueChars_bitvector(const string &str) {
 	//Reduce space usage by a factor of 8 using bitvector. 
 	//Each boolean otherwise occupies a size of 8 bits.
 	bitset<ALPHABET_SIZE> bits(0);
-	for(int i = 0; i < str.length(); i++) {
+	for (int i = 0; i < str.length(); i++) {
 		int val = str[i];
-		if(bits.test(val) > 0) {
+		if( bits.test(val) > 0) {
 			return false;
 		}
 		bits.set(val);
@@ -50,9 +51,9 @@ bool isUniqueChars_bitvector(const string &str) {
 character in the string against every other character. The time complexity is 
 O(n^2), but uses only O(1) additional space. */
 bool isUniqueChars_noDS_bruteforce(const string &str) {
-	for(int i = 0; i < str.length()-1; i++) {
-		for(int j = i+1; j < str.length(); j++) {
-			if(str[i] == str[j]) {
+	for (int i = 0; i < str.length()-1; i++) {
+		for (int j = i+1; j < str.length(); j++) {
+			if (str[i] == str[j]) {
 				return false;
 			}
 		}
@@ -67,11 +68,10 @@ take O(n log n) time, and the comparisongs take O(n). */
 bool isUniqueChars_noDS_sort(string str) {
 	/* Sort the string using heap sort. Unlike other O(n log n) sorting 
 	algorithms, heapsort does not require auxiliary space. */
-	
 	std::make_heap(str.begin(), str.end());
 	std::sort_heap(str.begin(), str.end());
 	
-	for(int i = 0; i < str.length()-1; i++) {
+	for (int i = 0; i < str.length()-1; i++) {
 		if (str[i] == str[i+1]) {
 			return false;
 		}
@@ -80,29 +80,54 @@ bool isUniqueChars_noDS_sort(string str) {
 }
 
 int main(){
-		vector<string> words = {"abcde", "hello", "apple", "kite", "padle"};
+	vector<string> words;
+	
+	/* Simple test cases. */
+	words.push_back("a"); //true
+	words.push_back("aa"); //false
+	words.push_back("ab"); //true
+	words.push_back("abcde"); //true
+	words.push_back("hello"); //false
+	words.push_back("abcdefghijklmnopqrstuvwxyzz"); //false
 		
-		for (auto word : words)
-		{
-			cout << word << string(": ") << boolalpha << isUniqueChars(word) <<endl;
-		}
-		
-		cout <<endl << "Using bit vector" <<endl;
-		for (auto word : words)
-		{
-			cout << word << string(": ") << boolalpha << isUniqueChars_bitvector(word) <<endl;
-		}
-		
-		cout <<endl << "Using no Data Structures, brute force method" <<endl;
-		for (auto word : words)
-		{
-			cout << word << string(": ") << boolalpha << isUniqueChars_noDS_bruteforce(word) <<endl;
-		}
-		
-		cout <<endl << "Using no Data Structures, sorting method" <<endl;
-		for (auto word : words)
-		{
-			cout << word << string(": ") << boolalpha << isUniqueChars_noDS_sort(word) <<endl;
-		}		
-		return 0;
+	/* Empty string or whitespace only test cases. */
+	words.push_back(""); //true
+	words.push_back(" "); //true
+	words.push_back("  "); //false
+	words.push_back("\t\n"); //true
+	words.push_back("\n\n"); //false
+	
+	/* Test all types of characters work (upper and lower alpha, numbers, 
+	symbols, and extended ASCII. */
+	words.push_back("ABC"); //true
+	words.push_back("ABCA"); //false
+	words.push_back("AaBbCc"); //true
+	words.push_back("1"); //true
+	words.push_back("12345"); //true
+	words.push_back("1234562"); //false
+	words.push_back("$#?^&"); //true
+	words.push_back("$#?^&$@"); //false
+	words.push_back("\x80\xC8\xFF"); //true
+	words.push_back("\x80\xC8\xFF\x80"); //false
+	
+	cout << endl << "Using boolean array" << endl;
+	for (auto word : words)	{
+		cout << word << string(": ") << boolalpha << isUniqueChars(word) << endl;
+	}
+	
+	cout << endl << "Using bit vector" << endl;
+	for (auto word : words)	{
+		cout << word << string(": ") << boolalpha << isUniqueChars_bitvector(word) << endl;
+	}
+	
+	cout << endl << "Using no Data Structures, brute force method" << endl;
+	for (auto word : words)	{
+		cout << word << string(": ") << boolalpha << isUniqueChars_noDS_bruteforce(word) << endl;
+	}
+	
+	cout << endl << "Using no Data Structures, sorting method" << endl;
+	for (auto word : words)	{
+		cout << word << string(": ") << boolalpha << isUniqueChars_noDS_sort(word) << endl;
+	}		
+	return 0;
 }
